e3e569fb428a673b179c903520e94e92cead3491
Irvin Denzel Torcuato <irvinisready@gmail.com>
2014-09-15 14:34:11 +0800

    data structure for processor load A.K.A process

---
+++ ninja.wiwi.simulation.cpu/src/ninja/wiwi/simulation/cpu/Load.java
@@ -0,0 +1,34 @@
+package ninja.wiwi.simulation.cpu;
+
+import java.io.Serializable;
+
+public class Load implements Serializable {
+
+	/**
+	 * Auto-generated serial version ID.
+	 */
+	private static final long serialVersionUID = 8189018485547041069L;
+
+	private String id;
+	private int load;
+	private int arrival;
+
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public int getLoad() {
+		return load;
+	}
+	public void setLoad(int load) {
+		this.load = load;
+	}
+	public int getArrival() {
+		return arrival;
+	}
+	public void setArrival(int arrival) {
+		this.arrival = arrival;
+	}
+}
